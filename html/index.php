002fa9b45262ca19d7f24c9a6748e46c59906d48
hderp4188 <hderp4188@gmail.com>
2024-04-07 07:11:00 +0000

    done

--- html/index.php
+++ html/index.php
@@ -1,56 +1,146 @@
 <?php
 include_once("/var/www/inc/dbinfo.inc");
 
-// Redirect the user if they're already in the database (Optional Step)
-// This part requires a session or cookie to identify returning users.
+// PHP functions for generating random usernames
+function getStringFromFile($file_name)
+{
+    $file_ = fopen($file_name, "r");
+    $arr = array();
+    $count = 0;
+    while(!feof($file_))
+    {
+        $str = ucfirst(fgets($file_));
+        $arr[$count++] = str_replace("\n", "", $str);
+    }
+
+    return $arr;
+}
+
+$nouns = getStringFromFile("nouns.txt");
+$adjs = getStringFromFile("adjectives.txt");
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Enter Username</title>
-<script>
-        // Function to generate a random username
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <style>
+        * {
+            box-sizing: border-box;
+            font-family: 'Roboto', sans-serif;
+        }
+        .container {
+            margin: auto;
+            margin-top: 10vh;
+            width: 35vw;
+            height: 30vh;
+            min-width: 530px;
+            min-height: 250px;
+            padding: 1rem;
+            padding-top: 0.5rem;
+            background-color: #3d3d3d;
+            letter-spacing: 0.75px;
+        }
+        .text-username, .text-small {
+            margin: auto;
+            margin-top: 0;
+            font-size: 1.2em;
+            font-weight: bold;
+            color: #fff;
+            text-align: center;
+        }
+        .center_inputs {
+            padding: 1rem;
+            margin: auto;
+            width: 60%;
+            min-width: 250px;
+            padding-top: 0.1vh;
+        }
+        .input-username {
+            width: 100%;
+            height: 15%;
+            font-size: 1em;
+            text-align: left;
+            line-height: 0;
+        }
+        .buttons-container {
+            width: 100%;
+            padding: 5%;
+            margin: auto;
+        }
+        .submit-username-btn, .randomize-username-btn {
+            width: 30%;
+            min-width: 150px;
+            height: 15%;
+            min-height: 40px;
+            margin: auto;
+            margin-top: 1vh;
+	    background-image: linear-gradient(45deg, #ff0077, #0f1669);
+        }
+        .randomize-username-btn {
+            background-image: linear-gradient(45deg, #ff0077, #0f1669);
+        }
+        .btn-text {
+            vertical-align: middle;
+            text-align: center;
+            color: #fff;
+        }
+    </style>
+    <script>
+        const nouns = <?php echo '["' . implode('", "', $nouns) . '"]' ?>;
+        const adjs = <?php echo '["' . implode('", "', $adjs) . '"]' ?>;
+        
         function generateRandomUsername() {
-            var randomUsername = "User" + Math.floor(Math.random() * 10000); // Example random username
-            document.getElementById("username").value = randomUsername; // Set the random username to the input field
-	
-	}
+            var num1 = Math.floor(Math.random() * nouns.length);
+            var num2 = Math.floor(Math.random() * adjs.length);
+            var randomUsername = adjs[num1] + nouns[num2] + Math.floor(Math.random() * 999);
+            document.getElementById("username").value = randomUsername;
+        }
 
-//duplicate checking
-	document.addEventListener('DOMContentLoaded', (event) => {
+        document.addEventListener('DOMContentLoaded', (event) => {
             document.getElementById('usernameForm').onsubmit = async function(e) {
-                e.preventDefault(); // Prevent the default form submission
+                e.preventDefault();
                 const formData = new FormData();
                 formData.append('username', document.getElementById('username').value);
-		console.log("Submitting form...");
-                // Fetch API to POST data and wait for the response
+                console.log("Submitting form...");
                 const response = await fetch('check_username.php', {
                     method: 'POST',
                     body: formData,
                 });
                 const result = (await response.text()).trim();
 
-                // Conditional actions based on the response
                 if (result === 'duplicate') {
                     alert('Username already exists. Please choose a different username.');
-		} else {	
-
-			// If unique, submit the form programmatically
+                } else {
                     this.submit();
                 }
             };
         });
     </script>
 </head>
-<body>
-    <form id = "usernameForm" action="welcome.php" method="POST" >
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-	<button type="submit" >Enter</button>
-	<button type="button" id="random" onclick="generateRandomUsername()">Random</button>
-    </form>
-	
 
+<body>
+    <div class="container">
+        <div class="text-username">
+            <p>Please Input A Username!</p>
+        </div>
+        <div class="center_inputs">
+            <form id="usernameForm" action="welcome.php" method="POST">
+                <div class="text-small">
+                    <p>Username:</p>
+                </div>
+                <input type="text" class="input-username" id="username" name="username" required placeholder="Input Username">
+                <div class="buttons-container">
+                    <button type="button" class="randomize-username-btn" onclick="generateRandomUsername()">
+                        <div class="btn-text">Randomize Username</div>
+                    </button>
+                    <button type="submit" class="submit-username-btn">
+                        <div class="btn-text">Enter</div>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
-
---
+++ html/index_old.php
@@ -0,0 +1,56 @@
+<?php
+include_once("/var/www/inc/dbinfo.inc");
+
+// Redirect the user if they're already in the database (Optional Step)
+// This part requires a session or cookie to identify returning users.
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Enter Username</title>
+<script>
+        // Function to generate a random username
+        function generateRandomUsername() {
+            var randomUsername = "User" + Math.floor(Math.random() * 10000); // Example random username
+            document.getElementById("username").value = randomUsername; // Set the random username to the input field
+	
+	}
+
+//duplicate checking
+	document.addEventListener('DOMContentLoaded', (event) => {
+            document.getElementById('usernameForm').onsubmit = async function(e) {
+                e.preventDefault(); // Prevent the default form submission
+                const formData = new FormData();
+                formData.append('username', document.getElementById('username').value);
+		console.log("Submitting form...");
+                // Fetch API to POST data and wait for the response
+                const response = await fetch('check_username.php', {
+                    method: 'POST',
+                    body: formData,
+                });
+                const result = (await response.text()).trim();
+
+                // Conditional actions based on the response
+                if (result === 'duplicate') {
+                    alert('Username already exists. Please choose a different username.');
+		} else {	
+
+			// If unique, submit the form programmatically
+                    this.submit();
+                }
+            };
+        });
+    </script>
+</head>
+<body>
+    <form id = "usernameForm" action="welcome.php" method="POST" >
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+	<button type="submit" >Enter</button>
+	<button type="button" id="random" onclick="generateRandomUsername()">Random</button>
+    </form>
+	
+
+</body>
+</html>
+
